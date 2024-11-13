6a67b46b2138968dd23c43c068cf30dc42214a25
ipalv9 <ipal7271@gmail.com>
2024-11-13 15:18:02 +0700

    memperbaiki link navbar dan membuat index jurusan

---
+++ jurusan/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+<?php INCLUDE('../navbar.php');
+?>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+            <div class="card-header">
+                <h3>Data Jurusan</h3>
+            </div>
+            <div class="card-body text-center">
+                <h4>Selamat Datang Di Data Jurusan</h4>
+                <p>Tetap Menyerah</p>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+<script src="../js/bootstrap.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
